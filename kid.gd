4456b43662802577d04130781677841b636b3e31
Jonathan Wehrle <jonathanw@gmx.de>
2015-08-23 17:05:57 +0200

    Kid notices the monster, gets frightened

--- kid.gd
+++ kid.gd
@@ -1,12 +1,61 @@
 extends AnimatedSprite
 
+export(int) var notice_threshold = 400 # max distance you can be noticed
+export(int) var max_notice = 90 # existence confirmed -> loss
+export(float) var light_probability_factor = 0.5
+export(int) var notice_gain = 600
+export(int) var notice_loss = 3
+export(int) var light_threshold = 30
+export(float) var light_multiplier = 4
+
+export(float) var fear_multiplier = 0.1
+export(float) var light_comfort = 0.5
+
 var notice = 0
 var fear = 0
 
+var light = false
+var queue_light = false
+
+func player_hidden():
+	#check if player is hidden
+	return false
 
 func turn_on_light( on ):
 	get_parent().get_node("Light").set_enabled( on )
+	light = on
 
 func _next_turn():
+	print("\n - - - next turn - - - ")
 	var dist = get_pos().distance_to(get_parent().get_node("Player").get_pos())
-	print( "Distance: ", dist )
+	var new_notice = notice
+	
+	if dist < notice_threshold and not player_hidden():
+		if light:
+			new_notice = min( notice + notice_gain * light_multiplier / dist, 100 )
+		else:
+			new_notice = min( notice + notice_gain / dist, 100 )
+	else:
+		new_notice = max( notice - notice_loss, 0 )
+	
+	if notice > 0:
+		fear += notice * fear_multiplier
+	if light and fear > 0:
+		fear = max(0, fear - light_comfort)
+	
+	if queue_light:
+		queue_light = false
+		turn_on_light(true)
+	elif notice > light_threshold and randf() < light_probability_factor * new_notice * 0.01:
+		print( "lighty light: ", light_probability_factor * new_notice * 0.01)
+		queue_light = true
+	
+	print("notice: ", new_notice)
+	print("fear: ", fear)
+	print("distance: ", dist)
+	print("gain: ", new_notice - notice)
+	if queue_light:
+		print("light queued")
+	
+	notice = new_notice
+
